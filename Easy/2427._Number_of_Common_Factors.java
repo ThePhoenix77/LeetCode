2938db7d4f9256b36c7f1db59e8c02453b44ce6c
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-08-25 21:21:06 +0100

    Create 2427._Number_of_Common_Factors.java

---
+++ Easy/2427._Number_of_Common_Factors.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count=0;
+        for(int i=1;i<=Math.min(a, b);i++){
+            if(a%i==0 && b%i==0) count++;
+        }
+        return count;
+    }
+}
